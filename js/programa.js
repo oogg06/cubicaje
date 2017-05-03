c463fa6d492e3c78c36dea761d084b989db126d1
oscar <oscar@example.com>
2017-05-03 08:32:45 +0200

    Incluido empaquetador

--- js/programa.js
+++ js/programa.js
@@ -1,24 +1,45 @@
-$(document).ready(inicio)
+$(document).ready(inicio);
 
-var informe     =   $("#informe")
-var largo_pale  =   $("#largo_pale")
-var ancho_pale  =   $("#ancho_pale")
+var informe     =   $("#informe")       ;
+var largo_pale  =   $("#largo_pale")    ;
+var ancho_pale  =   $("#ancho_pale")    ;
 
-var largo_caja  =   $("#largo_caja")
-var ancho_caja  =   $("#ancho_caja")
+var largo_caja  =   $("#largo_caja")    ;
+var ancho_caja  =   $("#ancho_caja")    ;
+
+var superficie_caja, superficie_pale    ;
+var posibles_cajas                      ;
+
+var dimensiones_cajas=new Array()       ;
+var empaquetador                        ;
 
 
-
+function calcular_cajas(){
+    superficie_pale=ancho_pale.val() * largo_pale.val()     ;
+    superficie_caja=ancho_caja.val() * largo_caja.val()     ;
+    posibles_cajas = superficie_pale / superficie_caja      ;
+    var cajas_teoricas=Math.trunc(posibles_cajas)           ;
+    for (var i=0; i<cajas_teoricas; i++){               
+        var caja={w:ancho_caja.val(), h:largo_caja.val()}   ;
+        dimensiones_cajas.push(caja)                        ;
+    }
+    empaquetador = new Packer()
+    alert(empaquetador)
+}
 
 function actualizar(){
+    calcular_cajas()
     var mm_ancho_pale=ancho_pale.val()
     var mm_largo_pale=largo_pale.val()
     
     var mm_ancho_caja=ancho_caja.val()
     var mm_largo_caja=largo_caja.val()
     
-    html_informe="Dimensiones pale:"+mm_ancho_pale+"mm x "+mm_largo_pale+"mm <br/>"
-    html_informe+="Dimensiones caja:"+mm_ancho_caja+"mm x "+mm_largo_caja+"mm <br/>"
+    html_informe  = "Dimensiones pale:"+mm_ancho_pale+"mm x "+mm_largo_pale+"mm <br/>";
+    html_informe += "Dimensiones caja:"+mm_ancho_caja+"mm x "+mm_largo_caja+"mm <br/>";
+    html_informe += "Superficie pale:"+superficie_pale+"mm2, superficie caja: "+superficie_caja+"mm2<br/>";
+    html_informe += "Número teórico de cajas:" + posibles_cajas;
+    html_informe += " (redondeado a   "+ Math.trunc(posibles_cajas) +")<br/>"
     informe.html(html_informe)
 }
 function inicio(){
@@ -35,5 +56,6 @@ function inicio(){
     $("#largo_caja").change(actualizar)
     $("#ancho_caja").change(actualizar)
     
+    actualizar()
     
 }
