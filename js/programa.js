2326354ddd6c333d6065826ef4bb16788c337e88
Oscar GG <profesor.oscar.gomez@gmail.com>
2017-05-03 12:51:30 +0200

    Ya dibuja

--- js/programa.js
+++ js/programa.js
@@ -3,37 +3,64 @@ $(document).ready(inicio);
 var informe     =   $("#informe")       ;
 var largo_pale  =   $("#largo_pale")    ;
 var ancho_pale  =   $("#ancho_pale")    ;
+var alto_pale   =   $("#alto_pale")    ;
 
 var largo_caja  =   $("#largo_caja")    ;
 var ancho_caja  =   $("#ancho_caja")    ;
+var alto_caja   =   $("#alto_caja")    ;
 
-var superficie_caja, superficie_pale    ;
-var posibles_cajas                      ;
 
-var dimensiones_cajas=new Array()       ;
-var empaquetador                        ;
 
+
+
+function get_numero(control){
+    return parseInt(control.val())
+}
 
 function calcular_cajas(){
-    superficie_pale=ancho_pale.val() * largo_pale.val()     ;
-    superficie_caja=ancho_caja.val() * largo_caja.val()     ;
-    posibles_cajas = superficie_pale / superficie_caja      ;
-    var cajas_teoricas=Math.trunc(posibles_cajas)           ;
-    for (var i=0; i<cajas_teoricas; i++){               
-        var caja={w:ancho_caja.val(), h:largo_caja.val()}   ;
+    //Cargamos el canvas y lo borramos
+    var canvas = document.getElementById("lienzo");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath()
+
+    var superficie_caja, superficie_pale    ;
+    var posibles_cajas                      ;
+    var int_ancho_pale  =   get_numero ( ancho_pale )    ;
+    var int_largo_pale  =   get_numero ( largo_pale )    ;
+    var int_ancho_caja  =   get_numero ( ancho_caja )    ;
+    var int_largo_caja  =   get_numero ( largo_caja )    ;
+    
+    superficie_pale=int_ancho_pale * int_largo_pale ;
+    superficie_caja=int_ancho_caja * int_largo_caja ;
+    var posibles_cajas = superficie_pale / superficie_caja      ;
+    var num_cajas_teoricas=Math.trunc(posibles_cajas)           ;
+    var dimensiones_cajas=[];
+    for (var i=0; i<num_cajas_teoricas; i++){               
+        var caja={w:int_ancho_caja,
+                    h:int_largo_caja }   ;
         dimensiones_cajas.push(caja)                        ;
     }
-    empaquetador = new Packer()
-    alert(empaquetador)
+    var empaquetador = new Packer(ancho_pale.val(), largo_pale.val());
+    dimensiones_cajas.sort(function(a,b) { return (b.h < a.h); });
+    empaquetador.fit(dimensiones_cajas);
+
+    for (var n=0; n<dimensiones_cajas.length; n++){
+        var c=dimensiones_cajas[n];
+        if (c.fit) {
+            ctx.rect(c.fit.x, c.fit.y, c.w, c.h);
+            ctx.stroke()
+        }
+    }
 }
 
 function actualizar(){
-    calcular_cajas()
-    var mm_ancho_pale=ancho_pale.val()
-    var mm_largo_pale=largo_pale.val()
+    calcular_cajas();
+    var mm_ancho_pale=ancho_pale.val();
+    var mm_largo_pale=largo_pale.val();
     
-    var mm_ancho_caja=ancho_caja.val()
-    var mm_largo_caja=largo_caja.val()
+    var mm_ancho_caja=ancho_caja.val();
+    var mm_largo_caja=largo_caja.val();
     
     html_informe  = "Dimensiones pale:"+mm_ancho_pale+"mm x "+mm_largo_pale+"mm <br/>";
     html_informe += "Dimensiones caja:"+mm_ancho_caja+"mm x "+mm_largo_caja+"mm <br/>";
@@ -46,15 +73,19 @@ function inicio(){
     informe     =   $("#informe")
     largo_pale  =   $("#largo_pale")
     ancho_pale  =   $("#ancho_pale")
+    alto_pale  =   $("#alto_pale")
     
     largo_caja  =   $("#largo_caja")
     ancho_caja  =   $("#ancho_caja")
+    alto_caja  =   $("#alto_caja")
     
     $("#largo_pale").change(actualizar)
     $("#ancho_pale").change(actualizar)
+    $("#alto_pale").change(actualizar)
     
     $("#largo_caja").change(actualizar)
     $("#ancho_caja").change(actualizar)
+    $("#alto_caja").change(actualizar)
     
     actualizar()
     
